4305decfaee84a408aa99f71e81454164c20eac5
rafiftrlh <rafifitro89@gmail.com>
2024-08-06 13:25:14 +0700

    feat: force delete staff

--- controllers/system_user.controller.js
+++ controllers/system_user.controller.js
@@ -1,13 +1,13 @@
-import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client"
+import bcrypt from "bcrypt"
 
-const prisma = new PrismaClient();
-const saltRounds = 10;
+const prisma = new PrismaClient()
+const saltRounds = 10
 
 export const createStaff = async (req, res) => {
   try {
-    const { name, email, password, role } = req.body;
-    const hashPassword = await bcrypt.hash(password, saltRounds);
+    const { name, email, password, role } = req.body
+    const hashPassword = await bcrypt.hash(password, saltRounds)
 
     await prisma.system_Users.create({
       data: {
@@ -16,32 +16,37 @@ export const createStaff = async (req, res) => {
         password: hashPassword,
         role: role,
       }
-    });
+    })
 
-    res.json({ msg: `Created a staff account with the name ${name} successfully` });
+    res.json({ msg: `Created a staff account with the name ${name} successfully` })
   } catch (error) {
     res.status(400).json({
       msg: `Failed to create a staff account`,
       err: error.message
-    });
+    })
   }
-};
+}
 
 export const softDeleteStaff = async (req, res) => {
   try {
-    const { id } = req.body;
+    const { id } = req.body
 
     const user = await prisma.system_Users.findFirst({
       select: {
-        name: true
+        name: true,
+        role: true
       },
       where: {
         id: id
       }
-    });
+    })
 
     if (!user) {
-      return res.status(404).json({ msg: "User not found" });
+      return res.status(404).json({ msg: "User not found" })
+    }
+
+    if (user.role == "ADMIN") {
+      return res.status(404).json({ msg: "User is admin" })
     }
 
     await prisma.system_Users.update({
@@ -51,20 +56,57 @@ export const softDeleteStaff = async (req, res) => {
       where: {
         id: id
       }
-    });
+    })
 
-    res.json({ msg: `Soft deleted a staff account with the name ${user.name} successfully` });
+    res.json({ msg: `Soft deleted a staff account with the name ${user.name} successfully` })
   } catch (error) {
     res.status(400).json({
       msg: `Failed to soft delete a staff account`,
       err: error.message
-    });
+    })
   }
-};
+}
+
+export const forceDeleteStaff = async (req, res) => {
+  try {
+    const { id } = req.body
+
+    const user = await prisma.system_Users.findFirst({
+      select: {
+        name: true,
+        role: true
+      },
+      where: {
+        id: id
+      }
+    })
+
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" })
+    }
+
+    if (user.role == "ADMIN") {
+      return res.status(404).json({ msg: "User is admin" })
+    }
+
+    await prisma.system_Users.delete({
+      where: {
+        id: id
+      }
+    })
+
+    res.json({ msg: `Force deleted a staff account with the name ${user.name} successfully` })
+  } catch (error) {
+    return res.status(400).json({
+      msg: `Failed to force delete a staff account`,
+      err: error.message
+    })
+  }
+}
 
 export const restoreStaff = async (req, res) => {
   try {
-    const { id } = req.body;
+    const { id } = req.body
 
     const user = await prisma.system_Users.findFirst({
       select: {
@@ -74,14 +116,14 @@ export const restoreStaff = async (req, res) => {
       where: {
         id: id
       }
-    });
+    })
 
     if (!user) {
-      return res.status(404).json({ msg: "User not found" });
+      return res.status(404).json({ msg: "User not found" })
     }
 
     if (!user.deletedAt) {
-      return res.status(400).json({ msg: "User is not deleted" });
+      return res.status(400).json({ msg: "User is not deleted" })
     }
 
     await prisma.system_Users.update({
@@ -91,13 +133,13 @@ export const restoreStaff = async (req, res) => {
       where: {
         id: id
       }
-    });
+    })
 
-    res.json({ msg: `Restored a staff account with the name ${user.name} successfully` });
+    res.json({ msg: `Restored a staff account with the name ${user.name} successfully` })
   } catch (error) {
     res.status(400).json({
       msg: `Failed to restore a staff account`,
       err: error.message
-    });
+    })
   }
-};
+}
--- routes/system_user.route.js
+++ routes/system_user.route.js
@@ -1,10 +1,11 @@
-import express from "express";
-import { createStaff, restoreStaff, softDeleteStaff } from "../controllers/system_user.controller.js"
+import express from "express"
+import { createStaff, forceDeleteStaff, restoreStaff, softDeleteStaff } from "../controllers/system_user.controller.js"
 
-const router = express.Router();
+const router = express.Router()
 
 router.post("/create-staff", createStaff)
 router.put("/soft-delete-staff", softDeleteStaff)
 router.put("/restore-staff", restoreStaff)
+router.delete("/force-delete-staff", forceDeleteStaff)
 
 export default router
